7748d6e800a6efd4ccf81196236f6b5deb7e383b
Suellen <suellenraysa@yahoo.com.br>
2022-03-09 16:39:59 -0300

    first commit

---
+++ src/server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+// app.get("/", function (request, response) {
+//   return response.send("Hello World");
+// });
+
+app.listen(3000, function () {
+    console.log("Rodando na porta 3000");
+  });
